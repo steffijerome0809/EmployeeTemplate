5177a5b228ba2744762b467d6aa29b265d11f885
Steffi Joel <steffijerome0809>
2020-06-11 01:14:57 -0400

    final script

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# EmployeeTemplate
+#Employee Template Engine:
+
+This application will generate a basic employee template.
+
+#Pre-Requisites:
+1.install npm init
+2.install npm i install
+3. Run the app by typing node app.js or node app 
+4. A set of questions will be prompted. Validations are made in the code so that email and phone number are submitted properly 
+5. Once all the questions are answered a employeesummary html is generated.
+6.Right click on the html page and open the chrome browser to view the employee template.
+
+Output:
+Images are added in the lib folder.
+
+Tests:
+
+Pre-requisites: 
+Install npm i -D jest
+Run npm test 
+
+The tests and the output are added as gifs under the main folder(Employee Summary)
+
+URL:
+https://github.com/steffijerome0809/EmployeeTemplate
---
+++ app.js
@@ -0,0 +1,220 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquire = require("inquirer");
+const fs = require("fs");
+
+var teamList = [];
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter manager name:",
+    validate: async (input) => {
+      if (input == "" || /\s/.test(input)) {
+        return "Please enter first or last name.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter manager's email:",
+    validate: async (input) => {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+        return true;
+      }
+      return "Please enter a valid email address.";
+    },
+  },
+  {
+    type: "input",
+    name: "officeNum",
+    message: "Enter office number:",
+    validate: async (input) => {
+      if (isNaN(input)) {
+        return "Please enter a number";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "hasTeam",
+    message: "Do you have any team members?",
+    choices: ["Yes", "No"],
+  },
+];
+
+const employeeQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter employee name:",
+    validate: async (input) => {
+      if (input == "") {
+        return "Please enter a name.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter their email:",
+    validate: async (input) => {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+        return true;
+      }
+      return "Please enter a valid email address.";
+    },
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is their role?",
+    choices: ["engineer", "intern"],
+  },
+  {
+    when: (input) => {
+      return input.role == "engineer";
+    },
+    type: "input",
+    name: "github",
+    message: "Engineer, enter your github username:",
+    validate: async (input) => {
+      if (input == "" || /\s/.test(input)) {
+        return "Please enter a valid GitHub username";
+      }
+      return true;
+    },
+  },
+  {
+    when: (input) => {
+      return input.role == "intern";
+    },
+    type: "input",
+    name: "school",
+    message: "Intern, enter your school name:",
+    validate: async (input) => {
+      if (input == "") {
+        return "Please enter a name.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "addAnother",
+    message: "Add another team member?",
+    choices: ["Yes", "No"],
+  },
+];
+
+function TeamList() {
+  inquire.prompt(employeeQuestions).then((employeeInfo) => {
+    if (employeeInfo.role == "engineer") {
+      var newMember = new Engineer(
+        employeeInfo.name,
+        teamList.length + 1,
+        employeeInfo.email,
+        employeeInfo.github
+      );
+    } else {
+      var newMember = new Intern(
+        employeeInfo.name,
+        teamList.length + 1,
+        employeeInfo.email,
+        employeeInfo.school
+      );
+    }
+    teamList.push(newMember);
+    if (employeeInfo.addAnother === "Yes") {
+      console.log(" ");
+      TeamList();
+    } else {
+      buildHtmlPage();
+    }
+  });
+}
+
+function buildHtmlPage() {
+  let newFile = fs.readFileSync("./templates/main.html");
+  fs.writeFileSync("./output/employeesummary.html", newFile, function (err) {
+    if (err) throw err;
+  });
+
+  console.log("Base page generated!");
+
+  for (member of teamList) {
+    if (member.getRole() == "Manager") {
+      buildHtmlCard(
+        "manager",
+        member.getName(),
+        member.getId(),
+        member.getEmail(),
+        "Office: " + member.getOfficeNumber()
+      );
+    } else if (member.getRole() == "Engineer") {
+      buildHtmlCard(
+        "engineer",
+        member.getName(),
+        member.getId(),
+        member.getEmail(),
+        "Github: " + member.getGithub()
+      );
+    } else if (member.getRole() == "Intern") {
+      buildHtmlCard(
+        "intern",
+        member.getName(),
+        member.getId(),
+        member.getEmail(),
+        "School: " + member.getSchool()
+      );
+    }
+  }
+  fs.appendFileSync(
+    "./output/employeesummary.html",
+    "</div></main></body></html>",
+    function (err) {
+      if (err) throw err;
+    }
+  );
+  console.log("Page tags closed! Operation completed.");
+}
+
+function buildHtmlCard(memberType, name, id, email, propertyValue) {
+  let data = fs.readFileSync(`./templates/${memberType}.html`, "utf8");
+  data = data.replace("nameHere", name);
+  data = data.replace("idHere", `ID: ${id}`);
+  data = data.replace(
+    "emailHere",
+    `Email: <a href="mailto:${email}">${email}</a>`
+  );
+  data = data.replace("propertyHere", propertyValue);
+  fs.appendFileSync("./output/employeesummary.html", data, (err) => {
+    if (err) throw err;
+  });
+  console.log("Card appended");
+}
+
+function generatePage() {
+  inquire.prompt(managerQuestions).then((managerInfo) => {
+    let teamManager = new Manager(
+      managerInfo.name,
+      1,
+      managerInfo.email,
+      managerInfo.officeNum
+    );
+    teamList.push(teamManager);
+    console.log(" ");
+    if (managerInfo.hasTeam === "Yes") {
+      TeamList();
+    } else {
+      buildHtmlPage();
+    }
+  });
+}
+
+generatePage();
